e003acde6b1b6ed6179153234b34be48df845678
Kasper Kain <kasperkain@gmail.com>
2023-08-16 13:53:46 -0500

    updated user modal

--- server/models/user.js
+++ server/models/user.js
@@ -10,6 +10,37 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+
+  // Strictly for UX/UI preferences
+  preferences: {
+    type: [
+      {
+        type: String,
+      },
+    ],
+    required: true,
+  },
+  profile: {
+    userName: { type: String, required: true },
+    favoriteGenre: { type: [{ type: String }] },
+
+    //Links to the ID of each of the users favorite author
+    //Placeholder for now
+    favoriteAuthors: { type: [{ type: Number }] },
+    // TODO: Figure out what to do here
+    socials: {},
+  },
+  accountDetails: {
+    //Will be populated by default values
+    paidUser: { type: Boolean, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    region: { type: String, required: true },
+    phoneNumber: { type: Number, required: true },
+    parentalControls: { type: Boolean, required: true },
+    paymentMethods: { type: [{ type: Object }] },
+    transactionHistory: { type: [{ type: Object }] },
+  },
 });
 
 module.exports = mongoose.model("User", UserSchema);
